7882ff36a693dac377d902294405dfbe754f04e9
galactrax <anthonycweathersby@gmail.com>
2020-02-10 08:32:14 -0700

    saving work

--- dev/scripts/plugin_wickdown.js
+++
@@ -1,9 +0,0 @@
-wick.plugin(wick.plugin.element, "comment", function(ele, lex){
-	
-	const marker = lex.copy();
-	const end = lex.copy();
-
-	while(!end.END && end.ty !== end.types.nl) end.next();
-
-	console.log(end.slice(marker));
-})
